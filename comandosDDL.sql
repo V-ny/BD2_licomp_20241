95f5b1da681c02a693fdb4e0f5bc311f25418ef3
V-ny <vinibm@outlook.com>
2024-03-21 20:53:50 -0300

    aula do dia 21/03

--- comandosDDL.sql
+++ comandosDDL.sql
@@ -12,3 +12,46 @@ use exemplodb;
 
 --excluindo um banco
 drop database exemplodb;
+
+create database bancoDB;
+
+use bancoDB;
+
+
+create table bancos(
+    numero int not null,
+    nome varchar(100) not null,
+    constraint bancos_pk primary key(numero)
+);
+
+create table agencias(
+    numero varchar(100) not null,
+    nome varchar(100) not null,
+    telefone int,
+    banco int not null,
+    constraint agencias_pk primary key(numero)
+);
+
+create table clientes(
+    id int(11) not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    constraint clientes_pk primary key(id)
+);
+
+alter table clientes add uf char(2) default 'RJ';
+alter table clientes add dia int default '01';
+alter table clientes add mes int default '01';
+
+
+
+-- configs do github
+-- definindo o usuario
+
+git init
+git config --global user.name ""
+git config --global user.email "vinibm@otlook.com"
+git add .
+git commit -m ""
+git remote add origin
+git push -u origin master
